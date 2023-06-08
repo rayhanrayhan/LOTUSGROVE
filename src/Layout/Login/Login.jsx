2056ad650edae2af4ab7df04c64e712a5b423518
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-06-08 21:06:59 +0600

    added the login page in google and facebook icon

--- src/Layout/Login/Login.jsx
+++ src/Layout/Login/Login.jsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet-async";
 import Lottie from "lottie-react";
 import lottiAnimation from "./../../../public/logsign.json";
 import { FiEye, FiEyeOff } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -24,9 +25,49 @@ const Login = () => {
             <h1 className="text-4xl font-bold">Login now!</h1>
             <Lottie className="w-max" animationData={lottiAnimation} />
           </div>
+
           {/* Form section */}
-          <div className="card flex-shrink-0 w-full md:w-auto shadow-2xl bg-base-100">
+          <form className="card flex-shrink-0 w-full md:w-auto shadow-2xl bg-base-100">
             <div className="card-body">
+              {/* social media btn  */}
+              <div className="">
+                {/* google  */}
+                <button
+                  type="button"
+                  className="py-2  mb-3 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                >
+                  <svg
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="mr-2"
+                    viewBox="0 0 1792 1792"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
+                  </svg>
+                  Sign in with Google
+                </button>
+
+                {/* facebook btn  */}
+                <button
+                  type="button"
+                  className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                >
+                  <svg
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="mr-2"
+                    viewBox="0 0 1792 1792"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z"></path>
+                  </svg>
+                  Sign in with Facebook
+                </button>
+              </div>
+              {/* form main body  */}
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -68,8 +109,17 @@ const Login = () => {
                 <button className="btn btn-primary">Login</button>
               </div>
             </div>
-          </div>
-          {/* Form section */}
+            <div className="mb-3 mx-auto">
+              <p>
+                Not a Member ??{" "}
+                <span className="text-pink-600">
+                  <Link>SignUp Now</Link>
+                </span>
+              </p>
+            </div>
+          </form>
+
+          {/* Form section end */}
         </div>
       </div>
     </div>

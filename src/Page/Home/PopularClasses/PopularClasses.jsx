7b8543b138bdbc68583e50284abd36cbd39bf426
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-06-10 18:38:19 +0600

    added classes six card

--- src/Page/Home/PopularClasses/PopularClasses.jsx
+++ src/Page/Home/PopularClasses/PopularClasses.jsx
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
+import PopularClassCard from "./PopularClassCard";
 
 const PopularClasses = () => {
-  const [PopulerClass, setPopularClass] = useState([]);
+  const [popularClasses, setPopularClasses] = useState([]);
+
   useEffect(() => {
     fetch("./classes.json")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setPopularClasses(data));
   }, []);
+
+  // Slice the first 6 cards
+  const currentCards = popularClasses.slice(0, 6);
+
   return (
     <div>
-      <h2> THis is popular section for assignment </h2>
+      <h2>This is the popular section for the assignment</h2>
+      <div className="grid grid-cols-3 gap-6">
+        {currentCards.map((classItem) => (
+          <PopularClassCard key={classItem._id} classItem={classItem} />
+        ))}
+      </div>
     </div>
   );
 };

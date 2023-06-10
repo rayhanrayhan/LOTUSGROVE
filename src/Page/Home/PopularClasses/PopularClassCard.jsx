853148a7893588315bf75e99479f939e37996824
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-06-10 20:21:10 +0600

    added populer classe

--- src/Page/Home/PopularClasses/PopularClassCard.jsx
+++ src/Page/Home/PopularClasses/PopularClassCard.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PopularClassCard = ({ classItem }) => {
-  const { image, price, instructorImage, seats, name, students } = classItem;
-  console.log(classItem);
+  const { image, price, instructorImage, des, seats, name, students } =
+    classItem;
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
+
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="my-6 flex justify-center">
+      <div className="card w-96 bg-base-100 md:shadow-xl md:hover:shadow-xl md:hover:drop-shadow-2xl">
         <figure>
           <img src={image} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">
-            Shoes!
-            <div className="badge badge-secondary">NEW</div>
-          </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+          <h2 className="card-title">{name}</h2>
+          <p>
+            {showFullDescription ? des : `${des.slice(0, 30)}...`}
+            {des.length > 30 && (
+              <button
+                className="text-blue-500 hover:underline"
+                onClick={toggleDescription}
+              >
+                {showFullDescription ? "See Less" : "See More"}
+              </button>
+            )}
+          </p>
+          <div className="card-actions justify-between">
+            <div className="">
+              Price: $<span className="text-red-600 font-bold">{price}</span>
+            </div>
+            <div className="">
+              Seats: <span className="text-red-600 font-bold">{seats}</span>
+            </div>
           </div>
         </div>
       </div>
--- src/Page/Home/PopularClasses/PopularClasses.jsx
+++ src/Page/Home/PopularClasses/PopularClasses.jsx
@@ -2,21 +2,27 @@ import React, { useEffect, useState } from "react";
 import PopularClassCard from "./PopularClassCard";
 
 const PopularClasses = () => {
-  const [popularClasses, setPopularClasses] = useState([]);
+  const [popularClass, setPopularClass] = useState([]);
 
   useEffect(() => {
     fetch("./classes.json")
       .then((res) => res.json())
-      .then((data) => setPopularClasses(data));
+      .then((data) => {
+        // Sort classes based on the number of students
+        const sortedClass = data.sort((a, b) => b.students - a.students);
+        setPopularClass(sortedClass);
+      });
   }, []);
 
   // Slice the first 6 cards
-  const currentCards = popularClasses.slice(0, 6);
+  const currentCards = popularClass.slice(0, 6);
 
   return (
-    <div>
-      <h2>This is the popular section for the assignment</h2>
-      <div className="grid grid-cols-3 gap-6">
+    <div className="container mx-auto px-4">
+      <div className="text-2xl md:text-4xl text-center font-bold py-8">
+        Popular Classes
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-center">
         {currentCards.map((classItem) => (
           <PopularClassCard key={classItem._id} classItem={classItem} />
         ))}

a0f71d45735f5f682465eeb4ab20500ce9df73bc
rayhanrayhan <mdrayhanuddinremon02@gmail.com>
2023-06-13 14:03:00 +0600

    my classes added data

---
+++ src/Components/MyClassCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { MdPayment } from "react-icons/md";
+import { AiFillEdit } from "react-icons/ai";
+const MyClassCard = ({ data, index }) => {
+  const { _id, email, classId, name, image, price, seats } = data;
+
+  const isEvenRow = index % 2 === 0;
+  const rowColorClass = isEvenRow ? "bg-gray-100" : "bg-gray-200";
+  return (
+    <tr key={classId} className={`${rowColorClass} border rounded-2xl`}>
+      <td className="px-4 py-2  text-center">{index + 1}</td>
+      <td className="px-4 py-2">
+        <img className="w-12 h-12 rounded-full" src={image} alt="" />
+      </td>
+      <td className="px-4 py-2 text-center">{name}</td>
+      <td className="px-4 py-2 text-center"> status</td>
+      <td className="px-4 py-2 text-center">student</td>
+      <td className="px-4 py-2 text-center">FEEDBACK</td>
+      <td className="px-4 py-2 text-center">
+        <div className=" ">
+          <button className="btn btn-circle ">
+            <AiFillEdit />
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
+};
+
+export default MyClassCard;
--- src/Pages/Dashboard/MyClasses.jsx
+++ src/Pages/Dashboard/MyClasses.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Helmet } from "react-helmet-async";
+import KeenSlider from "../KeenSlider/KeenSlider";
+
+import axios, { Axios } from "axios";
+import { toast } from "react-toastify";
+import { useQuery } from "@tanstack/react-query";
+import SelectedClassesData from "./SelectedClassesData";
+import MyClassCard from "../../Components/MyClassCard";
 
 const MyClasses = () => {
-    return (
-        <div>
-            MyClasses coming
+  //   const {
+  //     data: classData = [],
+  //     isLoading: loading,
+  //     refetch,
+  //   } = useQuery({
+  //     queryKey: ["classData"],
+  //     queryFn: async () => {
+  //       const res = await axios.get("http://localhost:5000/classes");
+  //       return res.data;
+  //     },
+  //   });
+
+  const {
+    data: classData = [],
+    isLoading: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["classData", { email: "user@example.com" }],
+    queryFn: async ({ queryKey }) => {
+      const [, params] = queryKey;
+      const email = params.email;
+      const res = await axios.get("http://localhost:5000/selectedClass", {
+        params: { email },
+      });
+      return res.data;
+    },
+  });
+
+  return (
+    <div className="container mx-auto border  text-center">
+      <Helmet>
+        <title>LOTUSGROVE || DASHBOARD</title>
+      </Helmet>
+      <KeenSlider keenTitle="MY Selected Classes" />
+      <div className="px-4">
+        <div className="grid my-6  gap-4">
+          <div className="col-span-1 md:col-span-3">
+            <div className="overflow-x-auto ">
+              <table className="w-full">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-2">SL No</th>
+                    <th className="px-4 py-2">PHOTO</th>
+                    <th className="px-4 py-2">CLASS NAME</th>
+                    <th className="px-4 py-2">STATUS</th>
+                    <th className="px-4 py-2">STUDENT</th>
+                    <th className="px-4 py-2">FEEDBACK</th>
+                    <th className="px-4 py-2">EDIT</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {classData.map((data, index) => (
+                    <MyClassCard key={data._id} data={data} index={index} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MyClasses;
